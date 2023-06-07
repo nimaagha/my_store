9776be758660bb829ee81cf015ef8dedfc3ecee9
Nima Aghaei <aghaei_nima@yahoo.com>
2023-06-07 07:01:16 -0700

    new entities

---
+++ Nima_Store.Domain/Entities/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nima_Store.Domain.Entities.Users
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<UserInRole> UserInRoles { get; set; }
+    }
+}
